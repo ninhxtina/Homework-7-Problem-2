60b894866a9662fa2b005eca123d9e26a83b8251
ninhxtina <christinaninh07@yahoo.com>
2020-03-10 20:56:08 -0700

    redid building heap

--- src/MaxHeap.java
+++ src/MaxHeap.java
@@ -1,4 +1,4 @@
-/**
+/** (https://www.geeksforgeeks.org/building-heap-from-array/)
  * (https://medium.com/@ankur.singh4012/implementing-max-heap-in-java-ea368dadd273)
  */
 
@@ -15,53 +15,19 @@ public class MaxHeap implements Heap {
         data = new Integer[size];
     }
 
-    //getters & setters
-    private int getLeftChildIndex(int parentIndex) {
-        return 2 * parentIndex + 1;
-    }
-
-    private int getRightChildIndex(int parentIndex) {
-        return 2 * parentIndex + 2;
-    }
-
-    private int getParentIndex(int childIndex) {
-        return (childIndex -1) /2;
-    }
-
-    private boolean hasLeftChild(int index) {
-        return getLeftChildIndex(index) < size;
-    }
-
-    private boolean hasRightChild(int index) {
-        return getRightChildIndex(index) < size;
-    }
-
-    private boolean hasParent(int index) {
-        return getParentIndex(index) >= 0;
-    }
-
-    private int leftChild(int parentIndex) {
-        return data[getLeftChildIndex(parentIndex)];
-    }
-
-    private int rightChild(int parentIndex) {
-        return data[getRightChildIndex(parentIndex)];
-    }
-
-    private int parent(int childIndex) {
-        return data[getParentIndex(childIndex)];
-    }
-
-    private void swap(int index1, int index2) {
-        int element = data[index1];
-        data[index1] = data[index2];
-        data[index2] = element;
-    }
-
     // build a heap based on data
     // to be implemented in O(nlogn)
     public void MaxHeapLogN(Integer[] data) {
         // homework
+        int n = data.length;
+        //index of last non-leaf node
+        int start = (n/2) -1;
+
+        //perform reverse level order traversal from
+        //last non-leaf node and heapify each node
+        for(int i = start;i>=0;i--) {
+            heapify(data,n,i);
+        }
     }
 
     // build a heap based on data
@@ -73,43 +39,10 @@ public class MaxHeap implements Heap {
     // add an item to the heap
     public boolean add(Integer item) {
         // homework
-        ensureCapacity();
-        data[size] = item;
-        size++;
-        heapifyUp();
-        return false; //placeholder
+        return false;
     }
 
-    private void ensureCapacity() {
-        if(size == capacity) {
-            data = Arrays.copyOf(data,capacity * 2);
-            capacity = capacity * 2;
-        }
-    }
 
-    private void heapifyUp() {
-        int index = size - 1;
-        while(hasParent(index) && parent(index) < data[index]) {
-            swap(getParentIndex(index),index);
-            index = getParentIndex(index);
-        }
-    }
-
-    private void heapifyDown() {
-        int index = 0;
-        while(hasLeftChild(index)) {
-            int smallestChildIndex = getLeftChildIndex(index);
-            if(hasRightChild(index) && rightChild(index) < leftChild(index)) {
-                smallestChildIndex = getRightChildIndex(index);
-            }
-            if(data[index] < data[smallestChildIndex]) {
-                swap(index,smallestChildIndex);
-            } else {
-                break;
-            }
-            index = smallestChildIndex;
-        }
-    }
     // return the max item in the heap
     public Integer get() {
         // homework
@@ -125,11 +58,32 @@ public class MaxHeap implements Heap {
         if(size == 0) {
             return null;
         }
-        int element = data[0];
-        data[0] = data[size -1];
-        size--;
-        heapifyDown();
         return null; //placeholder
+    }
+
+    private void heapify(Integer arr[], int n, int i) {
+        int largest = i;
+        int l = 2 * i + 1;
+        int r = 2 * i + 2;
+
+        //if left child is larger than root
+        if(l < n && arr[l] > arr[largest]) {
+            largest = l;
+        }
+
+        //if right child is larger than largest
+        if(r < n && arr[r] > arr[largest]) {
+            largest = r;
+        }
+
+        //if largest is not root
+        if(largest != i) {
+            int swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+        }
+        //recursively heapify the affected sub-tree
+        heapify(arr,n,largest);
     }
 
 
