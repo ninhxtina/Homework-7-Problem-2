96932632520255db3eaa1f905449c7975c67c6a4
ninhxtina <christinaninh07@yahoo.com>
2020-03-12 20:19:00 -0700

    changed maxheapn

--- src/MaxHeap.java
+++ src/MaxHeap.java
@@ -36,8 +36,9 @@ public class MaxHeap implements Heap {
     // to be implemented in O(n)
     public void MaxHeapN(Integer[] data) {
         // homework
-        int n = data.length;
-        heapifyUp();
+        for(int i = data.length / 2; i >= 0; i--) {
+            heapifyDown();
+        }
 
     }
 
